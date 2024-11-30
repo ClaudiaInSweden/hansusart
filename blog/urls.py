53e338ef9f950b6153f5aa0b3f1d4e1c366126b1
Claudia Bergstaller <claudia.bergstaller@gmx.com>
2024-11-30 22:56:38 +0000

    Add edit blogpost view, url and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-# from . import views
+from . import views
 from .views import BlogView, BlogDetailView
 
 
 urlpatterns = [
-    # path('', views.blog, name='blog'),
-    # path('<int:blog_id>/', views.blog_detail, name='blog_detail'),
-    # path('add/', views.add_blogpost, name='add_blogpost'),
     path('', BlogView.as_view(), name='blog'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('add/', views.add_blogpost, name='add_blogpost'),
+    path('edit/<int:post_id>/', views.edit_blogpost, name='edit_blogpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,15 +26,45 @@ def add_blogpost(request):
     if request.method == 'POST':
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
-            blogpost = form.save()
+            post = form.save()
             messages.success(request, 'The blog post has been added successfully!')
-            return redirect(reverse('blog_detail', args=[blog.id]))
+            return redirect(reverse('blog_detail', args=[post.id]))
         else:
             messages.error(request, 'Failed to add blogpost! Please make sure \
                 that the form is valid!')
     else:
         form = BlogForm()
-    template = 'blog/add_blogpost.html'
+    template = 'add_blogpost.html'
     context = {'form': form,}
+
+    return render(request, template, context)
+
+
+
+@login_required
+def edit_blogpost(request, post_id):
+    """ Edit a blogpost """
+    if not request.user.is_superuser:
+        messages.error(request, 'You are not authorized to perform this task.')
+        return redirect(reverse('home'))
+
+    post = get_object_or_404(Blog, pk=post_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Bloginlägget har uppdaterats!')
+            return redirect(reverse('blog_detail', args=[post.id]))
+        else:
+            messages.error(request, 'Failed to update blogpost! Please ensure \
+                that the form is valid!')
+    else:
+        form = BlogForm(instance=post)
+        
+    template = 'edit_blogpost.html'
+    context = {
+        'form': form,
+        'post': post,
+    }
 
     return render(request, template, context)
